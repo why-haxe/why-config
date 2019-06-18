bed7ae1a22d8bbd27903e49ae58e391bb4aef5db
Kevin Leung <kevinresol@gmail.com>
2019-06-18 18:42:24 +0800

    Expose constructor of EnvVar

--- src/why/config/EnvVar.hx
+++ src/why/config/EnvVar.hx
@@ -3,8 +3,7 @@ package why.config;
 using tink.CoreApi;
 
 class EnvVar implements Source {
-	public static var inst(default, null):EnvVar = new EnvVar();
-	function new() {}
+	public function new() {}
 	public function get(names:Array<String>):Promise<Array<String>> {
 		return names.map(Sys.getEnv);
 	}
